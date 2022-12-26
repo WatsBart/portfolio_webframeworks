7273b88da8338e17b9d570b61a1b77121130bee2
WatsBart <watsbart@gmail.com>
2022-12-26 20:26:48 +0100

    Added one more API call thing

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,11 +1,28 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { NavLink, Outlet } from "react-router-dom";
 import ThemeContext from "../styles/ThemeContext";
 
+interface dogApi {
+  message: string,
+  status: string
+}
+
 export const Root = () => {
+  const [src, setSrc] = useState("")
+  useEffect(()=>{
+    const getDog = async() => {
+      let result = await fetch("https://dog.ceo/api/breeds/image/random")
+      let json = await result.json()
+      setSrc(json.message)
+    }
+    getDog()
+  },[])
   const styles = useContext(ThemeContext)
   return (
     <div className="container">
+      <div className="dog">
+        <img src={src} />
+      </div>
       <div className="head">Web Frameworks</div>
       <div className="nav">
         <NavLink className={({ isActive }) => isActive ? "activeNavLink" : "navLink"} to="/" >Home</NavLink>
